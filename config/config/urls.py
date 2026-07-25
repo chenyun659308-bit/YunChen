d44bd0066aef0e722d626a3426242f6b09c99a51
ChenYun <chenyun659308@gmail.com>
2026-07-25 12:05:28 +0800

    修复favicon路由: 添加path捕获组

--- config/config/urls.py
+++ config/config/urls.py
@@ -8,8 +8,8 @@ def spa(r):
 urlpatterns = [path('admin/',admin.site.urls), path('api/',include('main.urls'))]
 from django.conf.urls.static import static
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += [re_path(r'^favicon\.ico$', serve, {'document_root': settings.FRONTEND_DIST_DIR})]
-urlpatterns += [re_path(r'^logo\.png$', serve, {'document_root': settings.FRONTEND_DIST_DIR})]
+urlpatterns += [re_path(r'^(?P<path>favicon\.ico)$', serve, {'document_root': settings.FRONTEND_DIST_DIR})]
+urlpatterns += [re_path(r'^(?P<path>logo\.png)$', serve, {'document_root': settings.FRONTEND_DIST_DIR})]
 urlpatterns += [re_path(r'^carousel/(?P<path>.*)$', serve, {'document_root': settings.FRONTEND_DIST_DIR.parent / 'public' / 'carousel'})]
 urlpatterns += [re_path(r'^products/(?P<path>.*)$', serve, {'document_root': settings.FRONTEND_DIST_DIR / 'products'})]
 urlpatterns += [re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': settings.FRONTEND_DIST_DIR / 'assets'})]
