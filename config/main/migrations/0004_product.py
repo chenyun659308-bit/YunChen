851a91c2f130c3bc87d8ab17289a2b2414c38a0d
ChenYun <chenyun659308@gmail.com>
2026-09-05 15:12:15 +0800

    Align product migration with server migration history

--- config/main/migrations/0004_product.py
+++ config/main/migrations/0004_product.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0002_alter_contract_options'),
+        ('main', '0003_alter_contract_id'),
     ]
 
     operations = [
